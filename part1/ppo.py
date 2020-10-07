faeb843e984cd5aa465ee861618a83f5370554d5
XiuliChen <XiuliChen618@gmail.com>
2020-10-07 13:31:27 +0300

    ppo.py: define our initial policy (actor); and value function (critic)

--- part1/ppo.py
+++ part1/ppo.py
@@ -1,7 +1,19 @@
+'''
+Step1: define our initial policy (actor); and value function (critic)
+	actor: input: obs; output: action
+	critic: input: obs; output: value 
+'''
 
 def PPO():
-	def __init__():
-		pass
+	def __init__(self, env):
+		# It is a openai gym env
+		self.env=env
+		# the observation and action dimension
+		self.obs_dim=env.observation_space.shape[0]
+		self.action_dim=env.action_space.shape[0]
+
+		self.actor=FeedforwardNN(self.obs_dim,self.action_dim)
+		self.critic=FeedforwardNN(self.obs_dim,1)
 
 	def learn(self, total_num_steps):
 		pass
